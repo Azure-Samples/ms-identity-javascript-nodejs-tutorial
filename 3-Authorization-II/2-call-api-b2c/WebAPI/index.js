c76bf7bd6a4b0b5079944b3009452cee1beab6d8
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-11-19 14:54:11 -0800

    update dependency

--- 3-Authorization-II/2-call-api-b2c/WebAPI/index.js
+++ 3-Authorization-II/2-call-api-b2c/WebAPI/index.js
@@ -4,7 +4,7 @@ const SERVER_PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const MsalExpressMiddleware = require('../../../MsalNodeCommons/MsalExpressMiddleware');
+const MsalExpressMiddleware = require('MsalNodeCommons/MsalExpressMiddleware');
 const auth = require('./auth.json');
 
 const msal = new MsalExpressMiddleware(auth);
