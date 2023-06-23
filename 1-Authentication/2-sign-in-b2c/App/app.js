094a5e28f3f5da178f15a3dd37f87562b159509d
Doğan Erişen <dogan.erisen@gmail.com>
2023-06-22 23:44:09 -0700

    remove creds

--- 1-Authentication/2-sign-in-b2c/App/app.js
+++ 1-Authentication/2-sign-in-b2c/App/app.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const { WebAppAuthProvider } = require('msal-node-wrapper');
 
 const mainController = require('./controllers/mainController');
+const authConfig = require('./authConfig');
 
 const SERVER_PORT = process.env.PORT || 4000;
 
@@ -43,27 +44,7 @@ async function main() {
     app.use(express.static(path.join(__dirname, './public')));
 
     // instantiate the wrapper
-    const authProvider = await WebAppAuthProvider.initialize({
-        authOptions: {
-            authority: "https://login.microsoftonline.com/cbaf2168-de14-4c72-9d88-f5f05366dbef/",
-            clientId: "82e01ead-82f8-4ec4-9c82-fea5347c33b2",
-            clientSecret: "yLl8Q~ssTFDlNRu~-PfrLwf32XCwEaVhyjJJhcov",
-            redirectUri: "/redirect",
-        },
-        systemOptions: {
-            loggerOptions: {
-                loggerCallback: (logLevel, message, containsPii) => {
-                    if (containsPii) {
-                        return;
-                    }
-
-                    console.log(message);
-                },
-                piiLoggingEnabled: false,
-                logLevel: 3,
-            },
-        }
-    });
+    const authProvider = await WebAppAuthProvider.initialize(authConfig);
 
     // initialize the wrapper
     app.use(authProvider.authenticate());
